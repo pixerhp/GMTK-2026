637f9fa82d532f5b035827ed7fc020c6294af31f
Rick Lastley <>
2026-07-22 14:42:32 -0600

    Added more functionality to the main menu

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -4,3 +4,10 @@ signal on_back_pressed
 
 func _on_back_button_pressed() -> void:
 	on_back_pressed.emit()
+
+
+func _on_full_screen_box_pressed() -> void:
+	if($SettingsVBox/FullScreenBox.button_pressed) :
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
