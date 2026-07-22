855f826852754b92eaa894fd888e473a57e6037e
Jcodefox <jcodefox@gmail.com>
2026-07-22 16:53:52 -0400

    Add dropping off ledges

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
+enum LEDGE_GRAB_DIRECTION { NEITHER, LEFT, RIGHT }
+
 @export var GRAVITY: float = 9.8
 @export var SPEED: float = 200.0
 @export var JUMP_VELOCITY: float = 300
 
 var time_since_on_floor: float = 10.0
 var time_since_pressed_jump: float = 10.0
+
+var ledge_grab_cooldown: float = 0.0
 
 @onready var ledge_left_upper_check: RayCast2D = $LedgeLeftUpperCheck
 @onready var ledge_left_lower_check: RayCast2D = $LedgeLeftLowerCheck
@@ -16,6 +20,9 @@ var time_since_pressed_jump: float = 10.0
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY
 	
+	ledge_grab_cooldown -= delta
+	ledge_grab_cooldown = max(ledge_grab_cooldown, 0.0)
+	
 	time_since_on_floor += delta
 	if is_on_floor():
 		time_since_on_floor = 0.0
@@ -23,18 +30,31 @@ func _physics_process(delta: float) -> void:
 	time_since_pressed_jump += delta
 	if Input.is_action_just_pressed("jump"):
 		time_since_pressed_jump = 0.0
+		ledge_grab_cooldown = max(ledge_grab_cooldown, 0.01)
 	
 	if time_since_pressed_jump < 0.1 and time_since_on_floor < 0.1:
 		velocity.y = -JUMP_VELOCITY
 	
 	velocity.x = Input.get_axis("left", "right") * SPEED
 	
-	if ledge_left_lower_check.is_colliding() and not ledge_left_upper_check.is_colliding() and time_since_pressed_jump > 0.01:
-		velocity = Vector2.ZERO
-		time_since_on_floor = 0.0
+	var ledge_grabbing: LEDGE_GRAB_DIRECTION = LEDGE_GRAB_DIRECTION.NEITHER
+	
+	if ledge_left_lower_check.is_colliding() and not ledge_left_upper_check.is_colliding():
+		ledge_grabbing = LEDGE_GRAB_DIRECTION.LEFT
 		
-	if ledge_right_lower_check.is_colliding() and not ledge_right_upper_check.is_colliding() and time_since_pressed_jump > 0.01:
+	if ledge_right_lower_check.is_colliding() and not ledge_right_upper_check.is_colliding():
+		ledge_grabbing = LEDGE_GRAB_DIRECTION.RIGHT
+	
+	if ledge_grabbing != LEDGE_GRAB_DIRECTION.NEITHER and ledge_grab_cooldown <= 0.0:
 		velocity = Vector2.ZERO
 		time_since_on_floor = 0.0
+		if Input.is_action_pressed("down"):
+			ledge_grab_cooldown = 0.1
+		if ledge_grabbing == LEDGE_GRAB_DIRECTION.LEFT:
+			if Input.is_action_just_pressed("right"):
+				ledge_grab_cooldown = 0.1
+		elif ledge_grabbing == LEDGE_GRAB_DIRECTION.RIGHT:
+			if Input.is_action_just_pressed("left"):
+				ledge_grab_cooldown = 0.1
 		
 	move_and_slide()
