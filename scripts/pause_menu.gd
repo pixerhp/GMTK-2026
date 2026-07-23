56db41d862231adc7eae8fbb9237197835ec58b9
Jcodefox <jcodefox@gmail.com>
2026-07-22 20:02:45 -0400

    Add pause menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,14 +1,20 @@
 extends Control
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = not get_tree().paused
+		visible = get_tree().paused
+
 func _on_resume_button_pressed() -> void:
-	# TODO: Resume game
-	pass
+	get_tree().paused = false
+	visible = get_tree().paused
 
 func _on_settings_button_pressed() -> void:
 	_hide_all()
 	$Settings.visible = true
 
 func _on_menu_button_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_back_pressed() -> void:
