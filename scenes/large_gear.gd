88917a4220a6cb5dbfb3f430f5dcc4231fadace5
Stevnmc32 <18pucercowboy@gmail.com>
2026-07-23 20:04:43 +0200

    added large gear gameplay functionality

--- scenes/large_gear.gd
+++ scenes/large_gear.gd
@@ -1,13 +1,20 @@
-extends Node2D
+extends StaticBody2D
 
 @export var gearDirection: bool = false
+@export var gearPower: float = 4.0
 @onready var gearAnim = $AnimatedSprite2D
 # TODO: replace this ready function with when the counter goes to zero, please. - Ray
 # this makes the switching
 func _ready() -> void:
-	_switch_direction()
-	Globals.tickbeat.connect(_switch_direction)
-
-func _switch_direction() -> void:
+	_turn_gear()
+	Globals.tickbeat.connect(_turn_gear)
+	
+func _turn_gear() -> void:
 	if (Globals.tickbeat_count % 4) == 0:
 		gearAnim.play("gearTurn")
+		if (gearDirection):
+			constant_angular_velocity = gearPower;
+		else:
+			constant_angular_velocity = -gearPower;
+		await get_tree().create_timer(1.0/6.0).timeout
+		constant_angular_velocity = 0.0;
