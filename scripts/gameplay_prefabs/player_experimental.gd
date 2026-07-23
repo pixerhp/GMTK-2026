d881ae764cc789f7df381561bd70490261d31cc3
Pixer H. P. <99830732+pixerhp@users.noreply.github.com>
2026-07-23 15:33:38 -0400

    Create foundations of experimental player stuff

---
+++ scripts/gameplay_prefabs/player_experimental.gd
@@ -0,0 +1,4 @@
+extends CharacterBody2D
+
+# NOTE Experimental script with the aim to give the player better mechanics and movement than 
+# the original player script does. (May replace the original script if/when completed.)
