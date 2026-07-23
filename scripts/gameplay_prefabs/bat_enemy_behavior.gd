3efb81511ac4e25cdb85f67ff11bb03b4d75b144
RAY <UltraRay252@gmail.com>
2026-07-23 17:18:04 -0400

    temporarily building on the bats behavior code

--- scripts/gameplay_prefabs/bat_enemy_behavior.gd
+++ scripts/gameplay_prefabs/bat_enemy_behavior.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
 # choices for bat behaviors.
-@export_enum("Chase", "Horizontal Move", "Vertical Move") var behaviorChoice: int = 0
+@export_enum("Chase", "Horizontal Move", "Vertical Move") var behavior_choice: int = 0
 @onready var playerRange = $BatSprite/PlayerDetection
-var moveSpeed: int = 30
-var moveExecute: int = 0
-var moveDirection: bool = 0
+var move_speed: int = 30
+var move_execute: int = 0
+@export var move_direction: int = 1 ## 1 equals moving either right or up, -1 for opposite, please ensure this is not set to zero, I am unsure what happens.
 var target = null
-var inChase = false
+var in_chase = false
 
 var health: int = 2
 
@@ -18,42 +18,54 @@ func _ready() -> void:
 
 func _reset_move() -> void:
 	if (Globals.tickbeat_count % 4) == 0:
-		moveExecute = moveSpeed
+		move_execute = move_speed
 
 # TODO: on Behavior 1 and Behavior 2, ensure they follow as the Enum says, making a back and forth hard coded pattern of direction!
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if moveExecute >= 1:
-		moveExecute -= 1
-	if behaviorChoice == 0:
-		if inChase == true:
+	if move_execute >= 1:
+		move_execute -= 1
+	# Chase Behavior
+	if behavior_choice == 0:
+		if in_chase == true:
 			if target.global_position.x > global_position.x:
-				velocity.x += (moveExecute * 0.7)
+				velocity.x += (move_execute * 0.7)
 			if target.global_position.x < global_position.x:
-				velocity.x -= (moveExecute * 0.7)
+				velocity.x -= (move_execute * 0.7)
 			if target.global_position.y > global_position.y:
-				velocity.y += (moveExecute * 0.7)
+				velocity.y += (move_execute * 0.7)
 			if target.global_position.y < global_position.y:
-				velocity.y -= (moveExecute * 0.7)
-	velocity.x *= 0.9
-	velocity.y *= 0.9
-	if behaviorChoice == 1:
-		pass
-	if behaviorChoice == 2:
+				velocity.y -= (move_execute * 0.7)
+		elif in_chase == false:
+			velocity.x += randi_range(-move_execute, move_execute)
+			velocity.y += randi_range(-move_execute, move_execute)
+
+	
+	# Horizontal moving behavior.
+	if behavior_choice == 1:
+		velocity.x += (move_execute * move_direction)
+		if is_on_wall():
+			print("Collided")
+			move_direction * -1
+	# Vertical moving behavior.
+	if behavior_choice == 2:
 		pass
 		
+	# regardless of behavior, this is the decay of their movement.
+	velocity.x *= 0.9
+	velocity.y *= 0.9
 	move_and_slide()
 
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		target = body
-		inChase = true
+		in_chase = true
 
 func _on_player_detection_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		target = null
-		inChase = false
+		in_chase = false
 
 func damage_by_player() -> void:
 	health -= 1
