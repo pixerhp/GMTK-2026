01e1fbf1a26edc47e2a2f97a0f079016e6c0a0b3
Jcodefox <jcodefox@gmail.com>
2026-07-24 14:13:18 -0400

    Fix crash on interact with interactable

--- scripts/map/base_interactable.gd
+++ scripts/map/base_interactable.gd
@@ -3,7 +3,7 @@ extends Node
 @export var required_item_id: String
 signal on_interacted
 
-func damage_by_player():
+func damage_by_player(player: Node2D):
 	if !required_item_id.is_empty():
 		if !%Player.get_node("Inventory").try_use_item(required_item_id): return
 	on_interacted.emit()
