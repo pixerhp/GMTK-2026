549cb1592901b5a0f9ea92ceae9cc55af9e5f592
Jcodefox <jcodefox@gmail.com>
2026-07-24 14:41:09 -0400

    Start switching tick timer to get_ticks_msec

--- scripts/global_scripts/globals.gd
+++ scripts/global_scripts/globals.gd
@@ -1,25 +1,25 @@
 extends Node
 
 signal tickbeat
-var tickbeat_timer: Timer = Timer.new()
 const TICKBEAT_TIMER_DURATION: float = 0.5
 var tickbeat_count: int = 0 # (Can be used with modular arithmetic for doing things every n beats or similar.)
+
+var level_start_time: float = 0
+var next_beat_time: float = 0
 func _on_tickbeat_timer_timeout() -> void:
 	tickbeat_count += 1 
 	tickbeat.emit()
-	tickbeat_timer.start(TICKBEAT_TIMER_DURATION)
+	next_beat_time = Time.get_ticks_msec() + TICKBEAT_TIMER_DURATION * 1000
 	#print("TICKBEAT ", tickbeat_count)
 
 var is_fullscreen: bool = false
 func _ready() -> void:
-	# Prepare tickbeat timer:
-	tickbeat_timer.timeout.connect(_on_tickbeat_timer_timeout)
-	add_child(tickbeat_timer)
-	tickbeat_timer.start(TICKBEAT_TIMER_DURATION)
 	is_fullscreen = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
 
 var window_mode_pre_fullscreen: int = ProjectSettings.get_setting("display/window/size/mode")
 func _process(_delta):
+	if Time.get_ticks_msec() >= next_beat_time:
+		_on_tickbeat_timer_timeout()
 	# Handle global inputs:
 	if Input.is_action_just_pressed("fullscreen_toggle"):
 		set_fullscreen(not is_fullscreen)
