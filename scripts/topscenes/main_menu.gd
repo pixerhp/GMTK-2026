813e0ac72fbf416eb65a1e39b41eae7b96602a20
Pixer H. P. <99830732+pixerhp@users.noreply.github.com>
2026-07-25 22:38:08 -0400

    Bugfix titlescreen script

--- scripts/topscenes/main_menu.gd
+++ scripts/topscenes/main_menu.gd
@@ -8,20 +8,17 @@ func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/topscenes/level_one.tscn")
 
 func _on_settings_button_pressed() -> void:
-	_hide_all()
-	$Settings.visible = true
+	%MainMenuButtons.visible = false
+	%Settings.visible = true
 
 func _on_credits_button_pressed() -> void:
-	_hide_all()
+	%MainMenuButtons.visible = false
 	%Credits.visible = true
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 func _on_back_pressed() -> void:
-	_hide_all()
-	$MainMenuVBox.visible = true
-
-func _hide_all() -> void:
-	for child in get_children():
-		child.visible = false
+	%Settings.visible = false
+	%Credits.visible = false
+	%MainMenuButtons.visible = true
